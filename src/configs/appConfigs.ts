d9ec02834b2c62668d7b59c150f18afbe80794d6
Nisharga <kabir@nexlent.com>
2024-04-02 19:26:54 +0600

    feat: full setup

---
+++ src/configs/appConfigs.ts
@@ -0,0 +1,12 @@
+import { Roboto } from 'next/font/google';
+
+export const SITE_TITLE_DEFAULT = 'Karate | Best Karate Site.';
+export const SITE_TITLE_TEMPLATE_DEFAULT = `%s - Karate`;
+export const SITE_DESCRIPTION_DEFAULT = 'You will Learn Karate From Here';
+export const SITE_VERIFICATION_GOOGLE_DEFAULT =
+    'google-site-verification=adwdawdaw';
+
+export const FONT_DEFAULT = Roboto({
+    weight: '400',
+    subsets: ['latin']
+});
